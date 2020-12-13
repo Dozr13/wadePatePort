9695f476c0fbda7ef3d78e55797b3d6fcf8c02fe
Wade Pate <wadejp8@gmail.com>
2020-12-12 17:47:41 -0700

    Made some visual changes

--- script.js
+++ script.js
@@ -39,6 +39,24 @@ function setTheme(mode) {
     
 }
 
+//
+var mybutton = document.getElementById("topBtn");
+
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+//
+
 var scroll = new SmoothScroll('.intro-wrapper a[href*="#"]', {
     speed: 850
 });
